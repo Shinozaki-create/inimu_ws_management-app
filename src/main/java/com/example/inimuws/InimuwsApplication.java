36e42161906a7b941269c1fba0e759ab8f385441
DESKTOP-S3L5LC2\academia <202512_aw_shinozaki@x-ide.sakura.ne.jp>
2026-06-02 10:35:06 +0900

    初回コミット

---
+++ src/main/java/com/example/inimuws/InimuwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.inimuws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InimuwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InimuwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inimuws/InimuwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inimuws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InimuwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
